73b083c8d240e36f1318c8a5d5b6c69fa7e22c47
Christian Coulter <christiancoulter@Christians-MacBook-Air-2.local>
2020-04-07 17:08:46 -0600

    finished mvp

--- js/index.js
+++ js/index.js
@@ -33,9 +33,54 @@ strongVar.forEach(attr => attr.addEventListener('click', () => attr.style.backgr
 
 //scaling
 
-const grow = document.querySelectorAll('.content-pick > h4')
+// const grow = document.querySelectorAll('.content-pick > h4')
 
-grow.addEventListener('mouseenter', () => {
-  grow.style.transform = 'scale(1.2)';
-  grow.style.transition = 'transform 1s';
+// grow.addEventListener('mouseenter', () => {
+//   grow.style.transform = 'scale(1.2)';
+//   grow.style.transition = 'transform 1s';
+// })
+
+
+//alerts
+const alerting = document.querySelectorAll('header > h2')
+
+alerting.forEach(attr => attr.addEventListener('click', () => {
+    alert("You found a secret button!")
+}))
+
+//load
+const loading = document.querySelectorAll('body')
+
+loading.forEach(attr => attr.addEventListener('load', () => {
+    alert("Yay, everything loaded correctly!")
+}))
+
+
+window.addEventListener("keydown", event => {
+    if (event.key == 'a') {
+        document.body.style.background = 'aqua'
+        document.body.style.visibility = 'visible'
+    }
+    if (event.key == 'w') {
+        document.body.style.background = 'white'
+        document.body.style.visibility = 'visible'
+        alert('Found the lights!')
+    }
+    if (event.key == 'o') {
+        document.body.style.background = 'orange'
+        document.body.style.visibility = 'visible'
+    }
+    if (event.key == 'r') {
+        document.body.style.background = 'red'
+        document.body.style.visibility = 'visible'
+    }
+    if (event.key == 'y') {
+        document.body.style.background = 'yellow'
+        document.body.style.visibility = 'visible'
+    }
+    if (event.key == 'b') {
+        document.body.style.background = 'black'
+        document.body.style.visibility = 'hidden'
+        alert("Uh oh, you're about to turn out the lights. Quick, find them!")
+    }
 })
